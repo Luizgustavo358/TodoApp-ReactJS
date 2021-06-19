a10a52975074617e97e10b43d12a6fb43b1e9c3d
Luizgustavo358 <luiz.gustavo358@gmail.com>
2021-06-18 22:22:23 -0300

    page header

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós' />
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi semper dictum ipsum, at posuere sapien tempus at. In faucibus eu urna quis tincidunt. Donec mattis posuere lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus rhoncus purus eu pellentesque convallis. Nullam laoreet enim in tempor posuere. Morbi id mauris et lacus luctus malesuada.</p>
+        <h2>Missão e Visão</h2>
+        <p>Fusce mattis felis quis justo imperdiet elementum. In maximus ligula at arcu tristique, id vehicula enim cursus. Nulla venenatis tortor pharetra, iaculis ante vitae, tincidunt ante. Etiam aliquam erat sit amet semper lobortis. Phasellus a hendrerit arcu. Sed malesuada, odio eget elementum posuere, urna nibh efficitur urna, at lobortis lectus mauris eleifend est. Aliquam euismod eu erat nec malesuada. Maecenas vel gravida nunc. Integer est diam, placerat a blandit sed, dapibus sed urna. Pellentesque rutrum ante et leo commodo, placerat vulputate odio cursus.</p>
+        <h2>Imprensa</h2>
+        <p>Aliquam non tempor ex. Aliquam in volutpat leo. Phasellus malesuada nisi auctor risus venenatis lobortis. Curabitur sodales neque et ultricies commodo. Suspendisse egestas scelerisque ipsum, quis consequat augue mollis id. Proin laoreet dictum posuere. Quisque nunc ante, molestie in augue a, dapibus rutrum est. Curabitur viverra enim quis nisi vehicula elementum. Aliquam mi arcu, fermentum at egestas ac, lacinia et augue. Pellentesque id tellus tincidunt, tempor neque sed, volutpat tellus. Nulla sed leo vel magna commodo tincidunt a vel lectus. Nam eu ultricies nisl. Nam porttitor faucibus sodales. Morbi non pulvinar nisl. Maecenas mi purus, ultricies non cursus sed, lacinia a nisi. Sed ac luctus lorem.</p>
     </div>
 )
