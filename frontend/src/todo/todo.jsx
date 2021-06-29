d4b6aaa3e8d82732fb54631a90d70a7303992e7b
Luizgustavo358 <luiz.gustavo358@gmail.com>
2021-06-28 21:50:30 -0300

    consulta e exclusao

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -18,12 +18,29 @@ export default class Todo extends Component {
 
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+
+        this.refresh()
+    }
+
+    refresh() {
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(resp => this.setState({
+                ...this.state,
+                description: '',
+                list: resp.data
+            }))
     }
 
     handleAdd() {
         const description = this.state.description
         axios.post(URL, { description })
-            .then(resp => console.log('funcionou'))
+            .then(resp => this.refresh())
+    }
+
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
     }
 
     handleChange(e) {
@@ -41,7 +58,9 @@ export default class Todo extends Component {
                     handleAdd={this.handleAdd} 
                     description={this.state.description} 
                     handleChange={this.handleChange} />
-                <TodoList />
+                <TodoList 
+                    list={this.state.list} 
+                    handleRemove={this.handleRemove} />
             </div>
         )
     }
