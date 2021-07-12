2276ea7ecdc32dfb50e325360a1e742410b5d4e0
Luizgustavo358 <luiz.gustavo358@gmail.com>
2021-07-12 16:27:41 -0300

    marcar como concluido/pendente

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from "redux";
 
 import IconButton from '../template/iconButton'
+import { markAsDone, markAsPending } from "./todoActions";
 
 const TodoList = props => {
     const renderRows = () => {
@@ -15,13 +17,13 @@ const TodoList = props => {
                         style='success'
                         icon='check'
                         hide={todo.done}
-                        onClick={() => props.handleMarkAsDone(todo)}
+                        onClick={() => props.markAsDone(todo)}
                     />
                     <IconButton
                         style='warning'
                         icon='undo'
                         hide={ !todo.done }
-                        onClick={() => props.handleMarkAsPending(todo)}
+                        onClick={() => props.markAsPending(todo)}
                     />
                     <IconButton
                         style='danger'
@@ -50,5 +52,7 @@ const TodoList = props => {
 }
 
 const mapStateToProps = state => ({ list: state.todo.list })
+const mapDispatchToProps = dispatch =>
+    bindActionCreators({ markAsDone, markAsPending }, dispatch)
 
-export default connect(mapStateToProps)(TodoList)
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
